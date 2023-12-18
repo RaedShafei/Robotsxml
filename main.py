289f9d14a1268ca512f36aee2a9aab3cb32c4e58
RaedShafei <69829680+RaedShafei@users.noreply.github.com>
2023-12-18 11:06:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ target = input("Enter target url: ")
 file1 = f'{target}/robots.txt'
 file2 = f'{target}/sitemap.xml'
 
-d1 = wget.download(file1, '/Users/raedshafei/PycharmProjects/Robotsxml')
-d2 = wget.download(file2, '/Users/raedshafei/PycharmProjects/Robotsxml')
+d1 = wget.download(file1, 'path to save')
+d2 = wget.download(file2, 'path to save')
 
 
